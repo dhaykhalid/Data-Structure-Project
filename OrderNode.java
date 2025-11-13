069079d043251d13a62085b31b8aa95afe4190f7
DaliaAlotaibi <dafasa2005@gmail.com>
2025-11-13 22:27:47 +0300

    Change package name from csc112 to ph1

--- OrderNode.java
+++ OrderNode.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package csc112;
+package ph1;
+
+
+
 
 /**
  *
@@ -116,3 +115,4 @@ public class OrderNode {
     
       
 }
+
